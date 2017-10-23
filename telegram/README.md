d766e59f605e06f7f6935c38e4edbfb21aeae488
Roberto Dantas <7robertodantas@gmail.com>
2017-10-23 20:29:56 -0300

    Initial Telegram Bot

---
+++ telegram/README.md
@@ -0,0 +1,20 @@
+# Piggy Bot Telegram Bot
+
+### How to run
+#### Dependencies and environment
+```bash
+$ python3 -m venv .env # Creates virtual python environment
+$ source .env/bin/activate # Enter into the virtual environment
+$ pip3 install -r requirements.txt # Install dependencies
+```
+#### Start Server
+```bash
+$ TELEGRAM_TOKEN=<token_here> python3 app.py
+```
+
+### Actions
+
+Action | Description
+------------ | -------------
+/start | Shows the initial message
+/help | Shows the help menu
---
+++ telegram/app.py
@@ -0,0 +1,62 @@
+import os
+import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def start(bot, update):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(bot, update):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def message(bot, update):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    token = os.environ['TELEGRAM_TOKEN']
+
+    if not token:
+        raise Exception('TELEGRAM_TOKEN not defined in environment variable')
+    else:
+        logger.info('Starting telegram bot with Token %s', token)
+
+    updater = Updater(token)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    dp.add_handler(MessageHandler(Filters.text, message))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
